1dd30cb6e1c9e8a2f25a39dec3ac774eb02c1aeb
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-04 15:31:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const convertToRoman = require('./solution');
+const convertToRoman = require('./script');
 
 app.use(express.static(__dirname))
 
