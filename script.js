5d056a4dc132b0ea942541aca9a2e4f6567451c8
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-04 16:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 function convertToRoman(num) {
-  const symbols = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
-  //your code here      
-       if (isNaN(num))
-        return NaN;
-    var digits = String(+num).split(""),
-        key = ["C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "I","II","III","IV","V","VI","VII","VIII","IX"],
-        roman = "",
-        i = 3;
-    while (i--)
-        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
+  	const obj = {0:['M',1000], 1:['D', 500], 2:['C', 100], 3:['L', 50], 4:['X', 10], 5:['V', 5], 6:['I', 1]};
+
+	let arr = [];
+	for (let i in obj){
+		let count = 0;
+		if (num >= obj[i][1]){
+			count = Math.floor(num / obj[i][1]);
+			num -= count*obj[i][1];
+			if (count===4 && (obj[i][0]==='C'||obj[i][0]==='X'||obj[i][0]==='I')){
+				if(arr[arr.length-1]===obj[i-1][0]){
+					arr.pop();
+					arr.push(obj[i][0], obj[i-2][0]);
+					continue;	
+				}
+				arr.push(obj[i][0], obj[i-1][0]);
+				continue;
+			}
+			for (let j=1; j<=count; j++){
+				arr.push(obj[i][0])
+			}
+		} 
+	}
+	return arr.join('');
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
